5f6b6261835ab5858885e81ccf19dd71b46393ba
TajuddinMolla <taj.unicart@gmail.com>
2024-01-03 22:08:24 +0600

    Project Initial

---
+++ data/data.ts
@@ -0,0 +1,27 @@
+export const NAVBAR = [
+  {
+    id: 1,
+    name: "Home",
+    slug: "/",
+  },
+  {
+    id: 2,
+    name: "Resume",
+    slug: "/resume",
+  },
+  {
+    id: 3,
+    name: "Portfolio",
+    slug: "/portfolio",
+  },
+  {
+    id: 4,
+    name: "Blogs",
+    slug: "/blogs",
+  },
+  {
+    id: 5,
+    name: "Contact",
+    slug: "/contact",
+  },
+];
