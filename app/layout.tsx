8eb09744f965276f9ace1eda4a48c5c09413fb90
TajuddinMolla <taj.unicart@gmail.com>
2024-01-11 23:48:35 +0600

    layout update

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className=" bg-gradient-to-r from-white to-[#F3EEEA] ">
-        <main className="container flex gap-8 min-h-screen max-h-screen py-8 2xl:py-24 px-8 mx-auto">
+        <main className="container flex gap-8 min-h-screen max-h-screen py-8 2xl:py-24 px-4 md:px-8 mx-auto">
           <Sidebar />
-          <div className="w-full shadow-lg bg-white rounded-[20px] p-10 max-h-[calc(100vh-64px)] overflow-y-auto">
+          <div className="w-full shadow-lg bg-white rounded-[20px] p-4 md:p-10 max-h-[calc(100vh-64px)] overflow-y-auto">
             {children}
           </div>
         </main>
