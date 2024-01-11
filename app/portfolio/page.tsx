717e601ba67bc7c54e6bff6f4b0b1910ec8d5542
TajuddinMolla <taj.unicart@gmail.com>
2024-01-11 22:08:13 +0600

    Portfolio Update

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -13,7 +13,7 @@ export default function Portfolio() {
         <span>Logo Design</span>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-8 mt-8">
         <PortfolioItem
           img={
             "https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
