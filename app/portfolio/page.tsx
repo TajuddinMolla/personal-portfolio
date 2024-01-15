dd0f8ef5e824fd9b1d13b824963bae05777738a2
TajuddinMolla <taj.unicart@gmail.com>
2024-01-15 21:12:10 +0600

    Update

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,6 +1,16 @@
 import PortfolioItem from "@/components/PortfolioItem";
 import React from "react";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
+
 export default function Portfolio() {
   return (
     <div className="text-[#776B5D]">
@@ -57,6 +67,20 @@ export default function Portfolio() {
           category="UI/UX"
         />
       </div>
+
+      <Dialog>
+        <DialogTrigger>Open</DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your account
+              and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+
     </div>
   );
 }
